5ec3be94320d6430ee6d67870d54aec352256592
David Wagner <david.karl.wagner@live.com>
2017-03-06 16:44:55 -0500

    set up source code, time conversion class

---
+++ PROG2070_Assignment3_DavidWagner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Assignment 3 PROG 2070: Console App that converts time
+ * David Wagner
+ * 
+ * Revision History:
+ * 
+ * 3/6/2017 CREATED
+ * 
+ * 
+ * */
+
+namespace PROG2070_Assignment3_DavidWagner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ PROG2070_Assignment3_DavidWagner/TimeConversion.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Assignment 3 PROG 2070: Console App that converts time
+ * David Wagner
+ * 
+ * Revision History:
+ * 
+ * 3/6/2017 CREATED
+ * 
+ * 
+ * */
+
+namespace PROG2070_Assignment3_DavidWagner
+{
+    public static class TimeConversion
+    {
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">time value</param>
+        /// <param name="convertFrom"> time unit converting from (seconds, minutes, hours, days)</param>
+        /// <param name="convertTo"> time unit converting to (seconds, minutes, hours, days)</param>
+        /// <returns>converted time value</returns>
+        public static double Convert(double value, string convertFrom, string convertTo)
+        {
+            double convertedValue = 0;
+            string convertFromMod = "";
+            string convertToMod = "";
+
+
+
+
+
+            return convertedValue;
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private static string ModifyInput(string input)
+        {
+            string modifiedInput = "";
+
+
+
+
+
+            return modifiedInput;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="convertFrom"> time unit converting from (seconds, minutes, hours, days)</param>
+        /// <param name="convertTo"> time unit converting to (seconds, minutes, hours, days)</param>
+        /// <returns></returns>
+        private static double GetMultiplier(string convertFrom, string convertTo)
+        {
+
+            double multiplier = 0;
+
+
+
+
+            return multiplier;
+
+        }
+
+        
+
+
+
+
+    }
+}
